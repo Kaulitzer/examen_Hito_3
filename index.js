6408795ddb8f4f2fd9b25197e81af3b7e1a089f0
kaulitzer <kaulitzer001@gmail.com>
2024-05-21 11:52:20 -0400

    cambios

--- index.js
+++ index.js
@@ -20,3 +20,5 @@ app.use('/api/productos', verifyToken, adminOnly, productosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+// cambios
